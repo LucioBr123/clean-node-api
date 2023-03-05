6b111f397da545cbc65fffa4915c6a81724a97e9
Aecio_Lucio <aeciofilho12@gmail.com>
2023-03-05 14:03:54 -0300

    feat: ensure SignUpController return an error no n

--- src/presentetion/controllers/signup.ts
+++ src/presentetion/controllers/signup.ts
@@ -1,7 +1,8 @@
 ﻿export class SignUpController {
   handle (httpRequest: any): any {
     return {
-      statusCode: 400
+      statusCode: 400,
+      body : new Error('Missing param: name')
     }
   }
 }
