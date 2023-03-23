f485d48df5602f27b81a38ceea7e8c3112827916
Aecio_Lucio <aeciofilho12@gmail.com>
2023-03-22 21:45:08 -0300

    feat: problems stop production

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,9 +1,25 @@
 import { EmailValidatorAdapter } from './email-validator'
+import validator from 'validator'
+
+jest.mock('validator', () => ({
+  isEmail (): boolean {
+    return true
+  }
+}))
 
 describe('EmailValidator Adapter', () => {
   test('Shold return false if validator returns false', () => {
     const sut = new EmailValidatorAdapter()
+    jest.spyOn(validator, 'isEmail').mockImplementationOnce(false)
     const isValid = sut.isValid('invalid_email')
     expect(isValid).toBe(false)
+  })
+})
+
+describe('EmailValidator Adapter', () => {
+  test('Shold return true if validator returns true', () => {
+    const sut = new EmailValidatorAdapter()
+    const isValid = sut.isValid('valid_email')
+    expect(isValid).toBe(true)
   })
 })
